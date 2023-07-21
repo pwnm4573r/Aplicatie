ac35736ccb8c48c70b16dbfcfa388af78b2735e2
nicolascelma <nicolascelma@windowslive.com>
2023-07-21 23:53:02 +0300

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("C:/Users/priho/Downloads/PCEP/app/joc4punct0-firebase-adminsdk-xfbgc-8508e04e2f.json")
+cred = credentials.Certificate("/Users/mudvayne/Documents/joc4punct0-firebase-adminsdk-xfbgc-8508e04e2f.json")
 firebase_admin.initialize_app(cred)
